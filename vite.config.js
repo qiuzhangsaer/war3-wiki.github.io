bfafba3c08dfec7472cc1745b93ce52f9efdf9e0
qiuzhansgaer <qiuzhangsaer@qq.com>
2024-06-29 00:09:21 +0800

    War3 Wiki

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 
+console.log(process.env)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,7 +14,7 @@ export default defineConfig({
     }
   },
   define: {
-    'E_TITLE': process.env.title || '',
-    'E_EMAIL': process.env.email || '',
+    'E_TITLE': process.env.title,
+    'E_EMAIL': process.env.email,
   },
 })
